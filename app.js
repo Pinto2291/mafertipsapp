7f0860077d9c181162f2a30ff754bcf9d594ea72
Ignacio P <51886210+Pinto2291@users.noreply.github.com>
2023-06-06 17:11:53 -0400

    added files and images

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.hero-slider').slick({
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: true,
+    });
+});
